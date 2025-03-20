46ef9d16f15e8f1d4489db98060c1bb43f28a63b
SujankarthikM <132041297+SujankarthikM@users.noreply.github.com>
2025-03-20 18:38:55 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
-gunicorn -b 0.0.0.0:5000 app:app
+
+# Start the first Flask app on port 5000
+gunicorn -b 0.0.0.0:5000 app:app &
+
+# Start the second Flask app on port 5001
+gunicorn -b 0.0.0.0:5001 j:app &
+
+# Start the third Flask app on port 5002
+gunicorn -b 0.0.0.0:5002 app1:app &
+
+# Wait for all background processes to finish
+wait
